f0caca4dce9c038342f5396eb54610d340aed5e8
Sergey Matyushkov <serzh.matyushkov@gmail.com>
2018-03-25 09:53:28 +0300

    add delepe task form

--- flex/project/models.py
+++ flex/project/models.py
@@ -36,7 +36,7 @@ class Task(models.Model):
     start_date = models.DateField()
     duration = models.DurationField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
-    predecessor = models.IntegerField(null=True, blank=True )
+    predecessor = models.IntegerField(null=True, blank=True)
     # successor = models.IntegerField(null=True, blank=True)
     responsible = models.CharField(max_length=255)
     status = models.SmallIntegerField(choices=STATUS)
