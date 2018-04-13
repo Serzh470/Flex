e0ac56e840980bdc11188be2bb32dd892bf12ff4
Sergey Matyushkov <serzh.matyushkov@gmail.com>
2018-04-13 10:47:32 +0300

    complete datepicker

--- flex/flex/settings.py
+++ flex/flex/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'project',
-    'bootstrap',
-    'fontawesome',
+    'bootstrap_datepicker',
 ]
 
 MIDDLEWARE = [
--- flex/project/forms.py
+++ flex/project/forms.py
@@ -1,34 +1,62 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django import forms
 from .models import Project, Task, STATUS, User, TASK_TYPE
-from bootstrap_datepicker.widgets import DatePicker
-
 
 
 class TaskForm(forms.ModelForm):
     """
     Create new task object
     """
-    wbs_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'WBS код'}))
-    task_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control', 'placeholder': 'Задача или Веха'}),
-                             choices=TASK_TYPE)
-    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название'}))
-    description = forms.CharField(required=False, widget=forms.Textarea(
-        attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Описание'}))
-    # start_date = forms.DateField(
-    #     widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Старт', 'type': 'date'}))
+    wbs_code = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'WBS код'}
+        )
+    )
+    task_type = forms.ChoiceField(
+        widget=forms.Select(
+            attrs={'class': 'form-control', 'placeholder': 'Задача или Веха'}
+        ), choices=TASK_TYPE
+    )
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Название'}
+        )
+    )
+    description = forms.CharField(
+        required=False, widget=forms.Textarea(
+            attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Описание'}
+        )
+    )
     start_date = forms.DateField(
-        widget=DatePicker(options={"format": "mm.dd.yyyy"}))
+        widget=forms.DateInput(
+            attrs={'class': 'datepicker form-control'}
+        ), required=False
+    )
     duration = forms.DurationField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Продолжительность'}))
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Продолжительность'}
+        ), required=False
+    )
     end_date = forms.DateField(
-        widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Финиш', 'type': 'date'}))
-    responsible = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Исполнитель'}))
-    status = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control', 'placeholder': 'Статус'}),
-                             choices=STATUS)
+        widget=forms.DateInput(
+            attrs={'class': 'datepicker form-control'}
+        ), required=False
+    )
+    responsible = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Исполнитель'}
+        )
+    )
+    status = forms.ChoiceField(
+        widget=forms.Select(
+            attrs={'class': 'form-control', 'placeholder': 'Статус'}
+        ), choices=STATUS
+    )
     project = forms.ModelChoiceField(
-        queryset=Project.objects.all(), required=False, widget=forms.Select(attrs={
-            'class': 'form-control', 'placeholder': 'Входит в проект'}))
+        queryset=Project.objects.all(), required=False, widget=forms.Select(
+            attrs={'class': 'form-control', 'placeholder': 'Входит в проект'}
+        )
+    )
 
     class Meta:
         model = Task
@@ -44,7 +72,6 @@ class TaskForm(forms.ModelForm):
             'status',
             'project',
         ]
-
 
 
 class TaskCreate(CreateView):
@@ -66,9 +93,7 @@ class TaskDelete(DeleteView):
     success_url = '/mytasks/'
 
 
-
 class UserForm(forms.ModelForm):
-
 
     class Meta:
         model = User
@@ -83,6 +108,7 @@ class UserForm(forms.ModelForm):
             'other',
             'project'
         ]
+
 
 class UserCreate(CreateView):
     form_class = UserForm
